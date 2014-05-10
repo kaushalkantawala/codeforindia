ba836c8e0df981968983db292153f2266af8c208
Ronak Shah <ronak.shah.j@gmail.com>
2014-05-09 23:33:58 -0700

    Initial Push with the RescueMe iOS App code

---
+++ RescueMe/RescueMe/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  RescueMe
+//
+//  Created by Ronak Shah on 5/9/14.
+//  Copyright (c) 2014 DJ911. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RescueMe/RescueMe/RMViewController.m
@@ -0,0 +1,29 @@
+//
+//  RMViewController.m
+//  RescueMe
+//
+//  Created by Ronak Shah on 5/9/14.
+//  Copyright (c) 2014 DJ911. All rights reserved.
+//
+
+#import "RMViewController.h"
+
+@interface RMViewController ()
+
+@end
+
+@implementation RMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
